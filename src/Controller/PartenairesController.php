03c0cb63d675019eb504f2b3d76893d095903bc9
lilou <michellisa.75@gmail.com>
2020-06-09 18:22:05 +0200

    add categories to PartenairesController

--- src/Controller/PartenairesController.php
+++ src/Controller/PartenairesController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Partner;
+use App\Repository\CategoryPartnerRepository;
 use App\Repository\PartnerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,10 +13,11 @@ class PartenairesController extends AbstractController
     /**
      * @Route("/partenaires", name="partenaires", methods={"GET"})
      */
-    public function index(PartnerRepository $partnerRepository)
+    public function index(PartnerRepository $partnerRepository, CategoryPartnerRepository $categoryPartnerRepository)
     {
         return $this->render('partenaires/index.html.twig', [
             'partners' => $partnerRepository->findAll(),
+            'categories' => $categoryPartnerRepository->findAll(),
 
         ]);
     }
