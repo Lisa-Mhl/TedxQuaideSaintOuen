2db6312892d035f6c59fadf8996d8b39a5d6dc4a
Krilline <krillinee@gmail.com>
2020-06-10 21:21:39 +0200

    added email sending, email template, contact form, data is adding in database

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
 use App\Repository\CategoryPartnerRepository;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\PartnerRepository;
 
@@ -27,6 +34,44 @@ class HomeController extends AbstractController
         return $this->render('home/partners.html.twig', [
             'partners' => $partnerRepository->findAll(),
             'categories' => $categoryPartnerRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     * @param Request $request
+     * @param MailerInterface $mailer
+     * @return Response
+     * @throws TransportExceptionInterface
+     */
+    public function contact(Request $request, MailerInterface $mailer): Response
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $contact->setCreatedAt(new \DateTime());
+            $entityManager->persist($contact);
+            $entityManager->flush();
+
+            $email = (new TemplatedEmail())
+                ->from($this->getParameter('mailer_from'))
+                ->to($contact->getEmail())
+                ->subject("Nous avons bien reçu votre message !")
+                ->htmlTemplate('emails/notification.html.twig')
+                ->context([
+                    'contact' => $contact,
+                ]);
+
+            $mailer->send($email);
+
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('home/contact.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
